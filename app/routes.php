e989e969913d0e241620f97f0c54f56625b8588b
Cesar Zavala <cesarzavalamesta@g.harvard.edu>
2014-07-13 02:07:57 -0400

    Basic routes mapped and working

--- app/routes.php
+++ app/routes.php
@@ -16,6 +16,22 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
+Route::get('/lorem-ipsum/{numberOfParagraphs?}', function($numberOfParagraphs=5){
+	echo "<h2>Lorem Ipsum paragraphs</h2>";
+	$generator = new Badcow\LoremIpsum\Generator();
+	$paragraphs = $generator->getParagraphs($numberOfParagraphs);
+	echo implode('<p>', $paragraphs);
+});
+
+Route::get('/user-generator/{numberOfUsers?}', function($numberOfUsers=2){
+	echo "<h2>Users</h2>";
+	$faker = Faker\Factory::create();
+
+	for ($i=0; $i < $numberOfUsers; $i++) {
+		echo "<h3>".$faker->name."</h3>";
+ 		echo "<p>".$faker->address."</p>";
+	}	
+});
 
 Route::get('/test', function() {
 	echo "<h2>Lorem Ipsum paragraphs</h2>";
